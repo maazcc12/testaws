aece60ddf66baf0e10e1b6718437fea49166802f
maazcc12 <maazahmed@catalectconsulting.com>
2024-08-28 16:26:42 +0500

    m

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+handler = Mangum(app)
+
+@app.post('/')
+async def greet():
+    return {"output":"Hello"}
+
+@app.post('/bye')
+async def intro():
+    return {"output":"bye"}
+
+
